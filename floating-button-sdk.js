f9c5eee47ef61853a82d77536f2d31673b3c0e84
eunji9128 <eunji9128@gmail.com>
2024-07-15 15:01:52 +0900

    test: iframe container shrink test

--- floating-button-sdk.js
+++ floating-button-sdk.js
@@ -10,7 +10,7 @@ class FloatingButton {
                 console.log(res);
                 this.userId = res;
                 console.log('userId', this.userId);
-                this.chatUrl = `https://demo.gentooai.com/${this.clientId}/${this.userId}`;
+                this.chatUrl = `https://demo.gentooai.com/${this.clientId}/sdk/${this.userId}`;
             });
         console.log('this userId', this.userId);
         // Create floating button
