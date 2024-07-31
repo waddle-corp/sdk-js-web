71eb65efffc9e00a5c29eda51b2d545545c92876
eunji9128 <eunji9128@gmail.com>
2024-07-31 14:12:35 +0900

    fix: expanded button design

--- floating-button-sdk.js
+++ floating-button-sdk.js
@@ -112,6 +112,8 @@ class FloatingButton {
             this.expandedButton.innerText = '';
             this.expandedButton.style.width = '50px';
             this.expandedButton.style.padding = 0;
+            this.expandedButton.style.border = 'none';
+            this.expandedButton.style.boxShadow = 'none';
             if (this.iframeContainer.classList.contains('iframe-container-hide')) {
                 this.button.className = 'floating-button-common button-image';
             }
